8433b8baa406a3cb91f6472b9763a328dca7cfa3
Grisha <grisha.zhivot@bk.ru>
2026-01-13 19:20:24 +0300

    Add abstract modules, interfaces andfirst moduleimplementations

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,24 @@
 fun main() {
+//    val manager = ResourceManager()
+//    val minerrals = OutpostResource(1, "Minerals", 300)
+//    val gas  = OutpostResource(2, "Gas", 100)
+//    manager.add(minerrals)
+//    manager.add(gas)
+//    manager.printAll()
+//
+//    val bonus = minerrals.copy(amount = minerrals.amount + 50)
+//
+//    println("Копия миниралов с бонусом: $bonus")
     val manager = ResourceManager()
-    val minerrals = OutpostResource(1, "Minerals", 300)
-    val gas  = OutpostResource(2, "Gas", 100)
-    manager.add(minerrals)
-    manager.add(gas)
+    manager.add(OutpostResource(1, "Minerals", 120))
+    manager.add(OutpostResource(2, "Gas", 40))
+
+    val generator = EnergyGenerator()
+    val lab = ResearchLab()
+
+    generator.performAction(manager)
+    lab.performAction(manager)
+
+    println()
     manager.printAll()
-
-    val bonus = minerrals.copy(amount = minerrals.amount + 50)
-
-    println("Копия миниралов с бонусом: $bonus")
 }
---
+++ src/ResearchLab.kt
@@ -0,0 +1,13 @@
+class ResearchLab : OutpostModule("Иследовательская лаборатория") {
+    override fun performAction(manager: ResourceManager) {
+        val minerals = manager.get("Minerals")
+
+        if (minerals == null || minerals.amount < 30) {
+            println("Недостаточно минералов для исследования")
+            return
+        } else {
+            minerals.amount -= 30
+            println("Лаборатория проводит исследование (минералов -30)")
+        }
+    }
+}
