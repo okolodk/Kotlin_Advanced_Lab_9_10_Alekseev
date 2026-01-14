02934d6d3ba01831962ce3b3fcae8970f0d4af21
Grisha <grisha.zhivot@bk.ru>
2026-01-14 16:28:02 +0300

    learn sealed-class

--- src/example/SealedClass.kt
+++ src/example/SealedClass.kt
@@ -1,9 +1,24 @@
 package example
 
+sealed class  OrderStatus {
+    object  Created: OrderStatus()
+    object  Paid : OrderStatus()
+    object  Shipped : OrderStatus()
+
+    data class  Cancelled(val reason: String) : OrderStatus()
+}
 sealed class NetworkResult {
     data class Success(val data: String) : NetworkResult()
     data class  Error(val message: String, val code: Int): NetworkResult()
     object Loading : NetworkResult()
+}
+fun handleOrder(status: OrderStatus) {
+    when ( status ) {
+        OrderStatus.Created -> println("Заказ создан")
+        OrderStatus.Paid -> println("Заказ оплачен")
+        OrderStatus.Shipped -> println("Заказ отправлен")
+        is OrderStatus.Cancelled -> println("Отменён: ${status.reason}")
+    }
 }
 fun handleResult(result: NetworkResult) {
     when (result) {
@@ -19,11 +34,16 @@ fun handleResult(result: NetworkResult) {
     }
 }
 fun main() {
-    val success = NetworkResult.Success("Данные получены")
-    val error = NetworkResult.Error("Сервер не отвичает", 500)
-    val loading = NetworkResult.Loading
+//    val success = NetworkResult.Success("Данные получены")
+//    val error = NetworkResult.Error("Сервер не отвичает", 500)
+//    val loading = NetworkResult.Loading
+//
+//    handleResult(success)
+//    handleResult(error)
+//    handleResult(loading)
 
-    handleResult(success)
-    handleResult(error)
-    handleResult(loading)
+    handleOrder(status = OrderStatus.Created)
+    handleOrder(status = OrderStatus.Paid)
+    handleOrder(status = OrderStatus.Shipped)
+    handleOrder(status = OrderStatus.Cancelled(reason = "Нет товара на складе"))
 }
