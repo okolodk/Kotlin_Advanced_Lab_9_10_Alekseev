8e4f6232606745c754743f9a187a7118ee160787
Grisha <grisha.zhivot@bk.ru>
2026-01-14 16:02:52 +0300

    learn sealed-class

--- src/actions/ModuleAction.kt
+++ src/actions/ModuleAction.kt
@@ -1,5 +1,5 @@
 package actions
-
+import  resources.ResourceManager
 interface ModuleAction {
     fun execute(manager: ResourceManager)
 }
---
+++ src/example/SealedClass.kt
@@ -0,0 +1,29 @@
+package example
+
+sealed class NetworkResult {
+    data class Success(val data: String) : NetworkResult()
+    data class  Error(val message: String, val code: Int): NetworkResult()
+    object Loading : NetworkResult()
+}
+fun handleResult(result: NetworkResult) {
+    when (result) {
+        is NetworkResult.Success -> {
+                println("Успех: ${result.data}")
+        }
+        is NetworkResult.Error -> {
+            println("Ошибка ${result.code}: ${result.message}")
+        }
+        is NetworkResult.Loading -> {
+            println("Loading ...")
+        }
+    }
+}
+fun main() {
+    val success = NetworkResult.Success("Данные получены")
+    val error = NetworkResult.Error("Сервер не отвичает", 500)
+    val loading = NetworkResult.Loading
+
+    handleResult(success)
+    handleResult(error)
+    handleResult(loading)
+}
